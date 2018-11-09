85ef7b8ff9b21cd0b4ed7fe65a50224bdf788474
Manuel Tuero <manutuero@gmail.com>
2018-11-09 15:50:45 -0300

    Pass test 9.

--- finger_exercises/ecmascript/01 - spread/index.js
+++ finger_exercises/ecmascript/01 - spread/index.js
@@ -10,3 +10,10 @@ export function min(args) {
 export const copy = obj => (isArray(obj) ? [...obj] : { ...obj });
 
 export const reverseMerge = (arr1, arr2) => [...arr2, ...arr1];
+
+export const filterAttribs = obj => {
+  const copied = copy(obj);
+  delete copied.a;
+  delete copied.b;
+  return copied;
+};
