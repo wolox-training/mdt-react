28c84e8310e62eb887d91ed887929a7ac3f107ac
Manuel Tuero <manutuero@gmail.com>
2018-11-12 12:57:35 -0300

    Add a Square component.

---
+++ src/app/components/Square/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import styles from './styles.css';
+
+class Square extends Component {
+  render() {
+    return <button className={styles.square}>{/* TODO */}</button>;
+  }
+}
+
+export default Square;
