7230664375dec544a0e8e60beea6e97f2db0c49e
Manuel Tuero <manutuero@gmail.com>
2018-11-12 10:41:41 -0300

    Passed the second promises test.

--- finger_exercises/ecmascript/02 - promises/index.js
+++ finger_exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,14 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
 export function delay(time) {
-  return new Promise(resolve => setInterval(() => resolve(time), Date.now()));
+  return new Promise((resolve, reject) => setInterval(() => {
+    if (time < time * time) {
+      resolve(time);
+    } else {
+      const reason = new Error('This time is too much !');
+      reject(reason);
+    }
+  }), Date.now());
 }
 
 export function asyncDelay() {
