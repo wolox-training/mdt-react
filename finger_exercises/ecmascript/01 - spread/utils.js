1761a6cec40346854537858df0590a6a709f188c
Manuel Tuero <manutuero@gmail.com>
2018-11-13 10:42:52 -0300

    Used the export default in 'isArray' function.

--- finger_exercises/ecmascript/01 - spread/utils.js
+++ finger_exercises/ecmascript/01 - spread/utils.js
@@ -1,7 +1,5 @@
 // This have a linter problem, it says that we should prefer
 // export default
 // Would you like to fix this issue ?
-
-// eslint-disable-next-line import/prefer-default-export
-export const isArray = aPossibleArray => aPossibleArray && Array.isArray(aPossibleArray);
+export default isArray = aPossibleArray => aPossibleArray && Array.isArray(aPossibleArray);
 
